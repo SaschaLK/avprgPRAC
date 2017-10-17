a788f15d047af00a1c270d6d279193744fba1813
Kufahl <Sascha.Kufahl@haw-hamburg.de>
2017-10-17 12:57:00 +0200

    fixed task 7

--- A7/script.js
+++ A7/script.js
@@ -5,14 +5,26 @@ var sound = new Audio("sounds/sound.wav");
 var source = context.createMediaElementSource(sound);
 var convolver = context.createConvolver();
 
+
 source.connect(convolver);
 convolver.connect(context.destination);
 
+
 playButton.addEventListener("click", function(e){
     
+    var selection = document.getElementById("select").value;
     
-    
-//    var dropDown = document.getElementById("select");
-//    var selection = dropDown.options[selection.selectedIndex].value;
+    var request = new XMLHttpRequest();
+    request.open("GET",  "sounds/" + selection + ".wav", true);
+    request.responseType = "arraybuffer";
+
+    request.onload = function () {
+        var undecodedAudio = request.response;
+        context.decodeAudioData(undecodedAudio, function (buffer) {
+            convolver.buffer = buffer;
+            convolver.normalize = true;
+        });
+    };
+    request.send();
     sound.play();
 });
